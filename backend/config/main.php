7a8261153f8b60df9f7d2344e098833e0624a100
Viktor Shinkevich <supostatekx@gmail.com>
2017-06-04 22:19:58 +0300

    backend config refactor

--- backend/config/main.php
+++ backend/config/main.php
@@ -7,43 +7,71 @@ $params = array_merge(
 );
 
 return [
-    'id' => 'app-backend',
-    'basePath' => dirname(__DIR__),
+    'id'                  => 'app-backend',
+    'basePath'            => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
-    'bootstrap' => ['log'],
-    'modules' => [],
-    'components' => [
-        'request' => [
-            'csrfParam' => '_csrf-backend',
-        ],
-        'user' => [
-            'identityClass' => 'common\models\User',
-            'enableAutoLogin' => true,
-            'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
-        ],
-        'session' => [
-            // this is the name of the session cookie used for login on the backend
-            'name' => 'advanced-backend',
-        ],
-        'log' => [
-            'traceLevel' => YII_DEBUG ? 3 : 0,
-            'targets' => [
+    'bootstrap'           => ['log'],
+    'modules'             => [],
+    'components'          =>
+        [
+            'request'      =>
                 [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
+                    'csrfParam' => '_csrf-backend',
+
                 ],
-            ],
+            'user'         =>
+                [
+                    'identityClass'   => 'common\models\User',
+                    'enableAutoLogin' => true,
+                    'identityCookie'  =>
+                        [
+                            'name'     => '_identity-backend',
+                            'httpOnly' => true
+                        ],
+
+                ],
+            'session'      =>
+                [
+                    // this is the name of the session cookie used for login on the backend
+                    'name' => 'advanced-backend',
+
+                ],
+            'log'          =>
+                [
+                    'traceLevel' => YII_DEBUG ? 3 : 0,
+                    'targets'    =>
+                        [
+                            [
+                                'class' => 'yii\log\FileTarget',
+                                'levels' => ['error', 'warning'],
+                            ],
+                        ],
+                ],
+            'errorHandler' =>
+                [
+                    'errorAction' => 'site/error',
+                ],
+            'urlManager'   =>
+                [
+                    'enablePrettyUrl' => true,
+                    'showScriptName'  => false,
+                    'rules'           =>
+                        [
+                            ''         => 'site/index',
+                            '<action>' => 'site/<action>',
+                        ],
+                ],
+            'urlManagerFrontend' =>
+                [
+                    'enablePrettyUrl' => true,
+                    'showScriptName'  => false,
+                    'rules'           =>
+                        [
+                            ''         => 'site/index',
+                            '<action>' => 'site/<action>',
+
+                        ],
+                ],
         ],
-        'errorHandler' => [
-            'errorAction' => 'site/error',
-        ],
-        'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'rules' => [
-            ],
-        ],
-        
-    ],
-    'params' => $params,
+    'params'               => $params,
 ];
