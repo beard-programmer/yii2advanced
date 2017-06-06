337d647b44e908c51a2694f824fc6542868d3fe2
Viktor Shinkevich <supostatekx@gmail.com>
2017-06-07 00:31:22 +0300

    frontendurlmanager

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -51,7 +51,7 @@ AppAsset::register($this);
     }
     $menuItems[] = [
         'label' => 'Frontend',
-        'url'   => ''
+        'url'   => [Yii::$app->urlManagerFrontend->createUrl([])]
     ];
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
