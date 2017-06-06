119fe12b90244fd0de03f8da5ac6dd3e1d4d2b88
Viktor Shinkevich <supostatekx@gmail.com>
2017-06-07 00:35:47 +0300

    frontendurlmanager

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -52,7 +52,7 @@ AppAsset::register($this);
 
     $menuItems[] = [
         'label' => 'Frontend',
-        'url'   => [Yii::$app->urlManagerFrontend->getBaseUrl()]
+        'url'   => Yii::$app->urlManagerFrontend->getBaseUrl()
     ];
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
