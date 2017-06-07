15aa370183836f3fc6d68226f70f5ffd3f6ffdb1
Viktor Shinkevich <supostatekx@gmail.com>
2017-06-07 13:26:57 +0300

    users controller

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -36,8 +36,8 @@ AppAsset::register($this);
     ]);
     $menuItems = [
         ['label' => 'Home',   'url' => Yii::$app->homeUrl],
-        ['label' => 'Users',  'url' => ['/user/']],
-        ['label' => 'Orders', 'url' => ['/order/']],
+        ['label' => 'Users',  'url' => ['/user/index']],
+        ['label' => 'Orders', 'url' => ['/order/index']],
     ];
     if (Yii::$app->user->isGuest) {
         $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
